fbd4f2cf8ce7fbc4ad2fc619a9e3504b798e3faa
Raido Raagmaa <raido.raagmaa@nortal.com>
2022-07-12 15:55:16 +0300

    Fix configuring individual users

--- src/main/java/ee/test_gov/sid/mock/data/UserInfo.java
+++
@@ -1,8 +0,0 @@
-package ee.test_gov.sid.mock.data;
-
-import io.micronaut.core.annotation.Introspected;
-
-@Introspected
-public class UserInfo {
-    public DataProvider.UserResponseType responseType;
-}
